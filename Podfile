e3343257176233073f95a60970d6a431d08bd482
Tong ZHAN <zhantong1994@163.com>
2017-09-15 16:14:01 +0800

    实现加载文章到列表

---
+++ Podfile
@@ -0,0 +1,8 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '9.3'
+use_frameworks!
+
+target 'WeTwo' do
+  pod 'Alamofire', '~> 4.5'
+  pod 'SwiftyJSON'
+end
---
+++ WeTwo/ArticleTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  ArticleTableViewCell.swift
+//  WeTwo
+//
+//  Created by zhantong on 2017/9/15.
+//  Copyright © 2017年 Tong Zhan. All rights reserved.
+//
+
+import UIKit
+import SwiftyJSON
+
+class ArticleTableViewCell: UITableViewCell {
+    @IBOutlet weak var contentLabel: UILabel!
+    @IBOutlet weak var commentTableView: UITableView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+    func loadData(_ article: JSON) {
+        contentLabel.text = article["article"].string
+    }
+}
